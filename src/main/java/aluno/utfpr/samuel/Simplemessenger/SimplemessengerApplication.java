503f6af5ef42a16624083f446ec70a7ca538112a
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-03-15 19:04:05 -0300

    start

---
+++ src/main/java/aluno/utfpr/samuel/Simplemessenger/SimplemessengerApplication.java
@@ -0,0 +1,13 @@
+package aluno.utfpr.samuel.Simplemessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplemessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplemessengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aluno/utfpr/samuel/Simplemessenger/SimplemessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package aluno.utfpr.samuel.Simplemessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplemessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
