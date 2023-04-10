4c7929f0fd61a09cff8ab148beac4c795d5d2cbe
SamuelPatrickMeneses <samuelmeneses800@gmail.com>
2023-04-10 20:15:24 -0300

    type: add class in file: ./src/main/java/aluno/utfpr/samuel/Simplemessenger/entities/Chat.java new class: aluno.utfpr.samuel.Simplemessenger.entities.Chat

---
+++ src/main/java/aluno/utfpr/samuel/Simplemessenger/entities/Chat.java
@@ -0,0 +1,34 @@
+package aluno.utfpr.samuel.Simplemessenger.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Chat {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long chatId;    
+
+    private String name;
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
